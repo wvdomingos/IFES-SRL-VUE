b9654bcd8a7ef92b62ca6e5c0419360ef2f1f591
Wander Vilhalva Domingos <wander.domingos@selita.coop.br>
2020-03-19 09:10:24 -0300

    Arquivos iniciais do projeto vue.js

---
+++ src/views/Form-tst.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-form inline>
+      <label class="sr-only" for="inline-form-input-name">Nome</label>
+      <b-input id="inline-form-input-name" class="mb-2 mr-sm-2 mb-sm-0" placeholder="J"></b-input>
+
+      <label class="sr-only" for="inline-form-input-username">Usuário</label>
+      <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inline-form-input-username" placeholder="Username"></b-input>
+      </b-input-group>
+
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+
+      <b-button variant="primary">Save</b-button>
+    </b-form>
+  </div>
+</template>
